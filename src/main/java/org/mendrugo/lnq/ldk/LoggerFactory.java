27114212ad7d68bb7dd83d715aab7f8424751462
Galder Zamarreño <galder@redhat.com>
2022-02-15 14:00:27 +0100

    Implement proper logging of LDK

--- src/main/java/org/mendrugo/lnq/ldk/LoggerFactory.java
+++ src/main/java/org/mendrugo/lnq/ldk/LoggerFactory.java
@@ -1,23 +1,30 @@
 package org.mendrugo.lnq.ldk;
 
-import org.ldk.structs.Logger;
 import org.ldk.structs.Record;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 @ApplicationScoped
-public class LoggerFactory implements Logger.LoggerInterface
+public class LoggerFactory implements org.ldk.structs.Logger.LoggerInterface
 {
+    @Inject
+    org.jboss.logging.Logger log;
+
     @Produces
-    Logger logger()
+    org.ldk.structs.Logger logger()
     {
-        return Logger.new_impl(this);
+        return org.ldk.structs.Logger.new_impl(this);
     }
 
     @Override
     public void log(Record record)
     {
-        System.out.println("[LDK] " + record);
+        log.infof(
+            "[%s] %s"
+            , record.get_level()
+            , record.get_args()
+        );
     }
 }
