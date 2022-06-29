1f80663659cad2de0686211cd5bb36b29ec43553
Galder Zamarreño <galder@zamarreno.com>
2022-06-29 17:44:33 +0100

    Remove native test, prepare for first test

--- src/test/java/org/mendrugo/lnq/rest/NativeNodeResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.mendrugo.lnq.rest;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeNodeResourceIT extends NodeResourceTest {
-
-    // Execute the same tests but in native mode.
-}
